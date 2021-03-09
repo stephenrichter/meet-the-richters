ef95c1856bca9c84593c3d1714d7a62fc822ac5e
stephenrichter <stephenrichter15@gmail.com>
2021-03-09 09:32:53 -0800

    Basic layout WIP

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header class="sticky top-0 z-20">
+      <div class="container mx-auto flex justify-between py-3 px-4 sm:px-0">
+        <Link href="#"><h1 class="text-3xl font-extralight py-3">Meet the Richters</h1></Link>
+        <nav class="self-center uppercase">
+          <div class="rounded-full h-11 w-11 flex items-center justify-center border border-gray-300">
+            <img class="p-2" src="https://assets.website-files.com/5e7f419c0c4ef4901af6d061/5e916c9e5f8678313f57819d_drag_handle-24px.svg" />
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ pages/api/hello.js
@@ -0,0 +1,3 @@
+export default function handerl(req, res) {
+  res.status(200).json({ text: 'Hello world' })
+}
